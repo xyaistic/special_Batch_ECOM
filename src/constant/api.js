60c96b91acc636c67541e2f0461a3b503c24e086
Shoaibkhan-786 <sk38485868@gmail.com>
2024-11-18 15:06:02 +0530

    Changes

--- src/constant/api.js
+++ src/constant/api.js
@@ -1,6 +1,5 @@
 // fetch('https://fakestoreapi.com/products/category/jewelery')
 export const BaseUrl = 'https://3nflhm13-8000.inc1.devtunnels.ms/';
-export const createUser = "register/"
 export const getCategories = 'categories/'
 export const getAllProduct  = 'Product/'
 
