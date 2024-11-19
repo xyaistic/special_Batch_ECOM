0cca52ac046dcaf0e6a3aeee15dae0424185eec1
Shoail-23 <kshoail168@gmail.com>
2024-11-19 15:44:19 +0530

    Update Api.js

--- src/Constant/Api.js
+++ src/Constant/Api.js
@@ -1,6 +1,8 @@
-// fetch('https://fakestoreapi.com/products/category/jewelery')
-export const BaseUrl = 'https://3nflhm13-8000.inc1.devtunnels.ms/';
+// fetch('https://fakestoreapi.com/products/category/https://2jewelery')
+export const BaseUrl = 'https://27nqfr43-8000.inc1.devtunnels.ms/';
 export const getCategories = 'categories/'
-export const getAllProduct  = 'Product/'
+export const getAllProduct  = 'Products/'
+export const createUser = 'register/'
+export const userLogin = 'login/'
 
 
