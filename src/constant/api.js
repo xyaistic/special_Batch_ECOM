95cc69b0f361843c588e01bdc4bc4d870ac0921b
Shoail-23 <kshoail168@gmail.com>
2024-11-20 12:05:01 +0530

    Delete src/constant directory

--- src/constant/api.js
+++
@@ -1,6 +0,0 @@
-// fetch('https://fakestoreapi.com/products/category/jewelery')
-export const BaseUrl = 'https://3nflhm13-8000.inc1.devtunnels.ms/';
-export const getCategories = 'categories/'
-export const getAllProduct  = 'Product/'
-
-
